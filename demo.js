6fd7630f22cba34ee0c023be29a8d2d43cfc34ed
Rustum Zia <rustumz@rustum-pc.localdomain>
2022-09-17 13:35:44 -0400

    update demo

--- demo.js
+++ demo.js
@@ -8403,6 +8403,10 @@ var ASM_CONSTS = {
   }
   }
 
+  function _foo() {
+          console.log("FOO WAS CALLED hey");
+      }
+
   function _getentropy(buffer, size) {
       if (!_getentropy.randomDevice) {
         _getentropy.randomDevice = getRandomDevice();
@@ -8941,6 +8945,7 @@ var asmLibraryArg = {
   "fd_read": _fd_read,
   "fd_seek": _fd_seek,
   "fd_write": _fd_write,
+  "foo": _foo,
   "getentropy": _getentropy,
   "invoke_dii": invoke_dii,
   "invoke_i": invoke_i,
