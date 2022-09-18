39334d8081d7abe86393d76668c1e7d179db303a
Rustum Zia <rustumz@rustum-pc.localdomain>
2022-09-18 11:57:33 -0400

    emcc build -O2

--- build_emcc.sh
+++ build_emcc.sh
@@ -3,4 +3,5 @@
 em++ src/*.cpp -Wall -o demo.js \
     -sUSE_SDL=2 -sUSE_SDL_IMAGE=2 -sUSE_SDL_TTF=2 -sUSE_SDL_MIXER=2 \
     -sSDL2_IMAGE_FORMATS="['png']" -sNO_DISABLE_EXCEPTION_CATCHING \
-    --preload-file images --preload-file fonts --preload-file sounds
+    --preload-file images --preload-file fonts --preload-file sounds \
+    "$@"
