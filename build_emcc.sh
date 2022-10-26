54d23dc123269d85bda204ba0bbad5b609d67783
RZia <36330543+grassdne@users.noreply.github.com>
2022-10-26 11:16:31 -0400

    build_emcc define RUNTIME_BASE_PATH

--- build_emcc.sh
+++ build_emcc.sh
@@ -4,6 +4,7 @@ mkdir -p docs
 cd docs
 
 em++ ../src/*.cpp -Wall -o demo.js \
+    -D RUNTIME_BASE_PATH="" \
     -sUSE_SDL=2 -sUSE_SDL_IMAGE=2 -sUSE_SDL_TTF=2 -sUSE_SDL_MIXER=2 \
     -sSDL2_IMAGE_FORMATS="['png']" -sNO_DISABLE_EXCEPTION_CATCHING \
     --preload-file ../assets@assets \
