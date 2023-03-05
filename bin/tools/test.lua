3b0a10ed9400e768474ce4c331c2128089d49dd7
grassdne <ziarustum@gmail.com>
2023-03-05 09:23:09 -0500

    build test to separate executable

--- bin/tools/test.lua
+++ bin/tools/test.lua
@@ -6,13 +6,13 @@ local tests = {
     "tests/red",
 }
 
-if not os.execute("cmake . -DFRONTEND=TEST") then
-    return io.stderr:write("Could not run tests: CMake Failed\n")
+if not os.execute("cmake . -DFRONTEND=TEST && make -j") then
+    return io.stderr:write("Could not run tests: Failed to build testminesector\n")
 end
-if not os.execute("make -j") then
-    return io.stderr:write("Could not run tests: Make Failed\n")
+if not os.execute("cmake . -DFRONTEND=NATIVE && make -j") then
+    return io.stderr:write("Could not run tests: Failed to build minesector\n")
 end
 
 for _,file_name in ipairs(tests) do
-    os.execute(string.format("./minesector run %q", file_name))
+    os.execute(string.format("./testminesector run %q", file_name))
 end
