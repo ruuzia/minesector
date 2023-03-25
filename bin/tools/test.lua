5d24cbe193e25d1b2e9b3d420aa26deef3444c0f
grassdne <ziarustum@gmail.com>
2023-03-25 07:59:53 -0400

    [test.lua] handle signal interruptions

--- bin/tools/test.lua
+++ bin/tools/test.lua
@@ -6,13 +6,25 @@ local tests = {
     "tests/red",
 }
 
-if not os.execute("cmake . -DFRONTEND=TEST && make -j") then
+Exe = function(cmd)
+    print("+ "..cmd)
+    local success, result, code = os.execute(cmd)
+    if result == "signal" then
+        io.stderr:write("tests interrupted by signal "..tostring(code).."\n")
+        os.exit(1)
+    end
+    assert(result == "exit")
+    return success, code
+end
+
+if not Exe("cmake . -DFRONTEND=TEST && make -j") then
     return io.stderr:write("Could not run tests: Failed to build testminesector\n")
 end
-if not os.execute("cmake . -DFRONTEND=NATIVE && make -j") then
+if not Exe("cmake . -DFRONTEND=NATIVE && make -j") then
     return io.stderr:write("Could not run tests: Failed to build minesector\n")
 end
 
 for _,file_name in ipairs(tests) do
-    os.execute(string.format("./testminesector run %q", file_name))
+    local success, code = Exe(string.format("./testminesector run %q", file_name))
+    if not success then io.stderr:write("Test exited with code "..code.."\n") end
 end
