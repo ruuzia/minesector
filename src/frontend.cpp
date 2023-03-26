ffc873c7c07c368677923b661e7f3dc4b42d98d0
grassdne <ziarustum@gmail.com>
2023-03-26 12:01:27 -0400

    test_frontend move to c++

--- src/frontend.cpp
+++ src/frontend.cpp
@@ -1,7 +1,5 @@
 #if defined(FRONTEND_NATIVE)
 #include "native_frontend.cpp"
 #elif defined(FRONTEND_TEST) 
-extern "C" {
-    #include "test_frontend.c"
-}
+#include "test_frontend.cpp"
 #endif
--- src/test_frontend.cpp
+++ src/test_frontend.cpp
@@ -1,28 +1,35 @@
 #include <SDL_events.h>
 #include <SDL_stdinc.h>
 #include <SDL_timer.h>
-#include <assert.h>
-#include <errno.h>
-#include <stdbool.h>
+#include <cassert>
+#include <cstdbool>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include "backend.h"
 #include "frontend.h"
+
+
+constexpr uint32_t INTERVAL = 100;
+constexpr uint32_t AUTOQUIT_PERIOD = 5000;
 
 static enum { RUNNING, RECORDING, FINISHED } state;
 
 static struct {
-    FILE *expected;
-    FILE *file;
+    std::ofstream expected;
+    std::ofstream file;
 } recorder;
 
 static struct {
-    FILE *expected;
-    FILE *sim_input;
+    std::ifstream expected;
+    std::ifstream sim_input;
     bool completed;
     bool succeeded;
 } runner;
 
-static const char *name;
-static FILE *inital_savedata;
+static std::string name;
+static std::ifstream inital_savedata;
 
 static Uint32 quit_timer(Uint32 interval, void *param) {
     (void)param;
@@ -32,46 +39,37 @@ static Uint32 quit_timer(Uint32 interval, void *param) {
 }
 
 static void quit_in_a_bit(void) {
-    SDL_AddTimer(5000, quit_timer, NULL);
+    SDL_AddTimer(AUTOQUIT_PERIOD, quit_timer, NULL);
 }
 
 void closeSaveFile(void) {
-    if (inital_savedata) {
-        // Reader
-        fclose(inital_savedata);
-        inital_savedata = NULL;
+    if (inital_savedata.is_open()) {
+        inital_savedata.close();
         return;
     }
 
     switch (state) {
     case RECORDING:
-        assert(recorder.file != NULL);
-        assert(recorder.expected != NULL);
-        fclose(recorder.file);
-        fclose(recorder.expected);
-        recorder.file = NULL;
+        recorder.file.close();
+        recorder.expected.close();
         state = FINISHED;
         quit_in_a_bit();
         return;
 
     case RUNNING:
         if (runner.completed) {
-            printf("%s SUCCEEDED\n", name);
+            printf("%s SUCCEEDED\n", name.c_str());
         } else {
-            printf("%s FAILED\n", name);
+            printf("%s FAILED\n", name.c_str());
             
         }
-        fclose(runner.expected);
-        runner.expected = NULL;
+        runner.expected.close();
         state = FINISHED;
         quit_in_a_bit();
         return;
 
     case FINISHED:
         // do nothing
-        assert(recorder.file == NULL);
-        assert(recorder.expected == NULL);
-        assert(inital_savedata == NULL);
         return;
     }
 }
@@ -79,13 +77,12 @@ static int onEvent(void *userdata, SDL_Event *event) {
     (void)userdata;
     if (state == RECORDING && event->type == SDL_MOUSEBUTTONDOWN) {
         SDL_MouseButtonEvent *mouse = &event->button;
-        if (recorder.expected) return 1;
         if (mouse->button == SDL_BUTTON_LEFT) {
             onClick(mouse->x, mouse->y);
-            fprintf(recorder.file, "CLICK %d %d\n", mouse->x, mouse->y);
+            recorder.file << "CLICK " << mouse->x << " " << mouse->y << '\n';
         } else if (mouse->button == SDL_BUTTON_RIGHT) {
             onAltClick(mouse->x, mouse->y);
-            fprintf(recorder.file, "ALTCLICK %d %d\n", mouse->x, mouse->y);
+            recorder.file << "ALTCLICK " << mouse->x << " " << mouse->y << '\n';
         }
     } else if (state == FINISHED && event->type == SDL_KEYDOWN && event->key.keysym.sym == SDLK_RETURN) {
         quit();
@@ -95,40 +92,36 @@ static int onEvent(void *userdata, SDL_Event *event) {
 
 bool openSaveReader(void) {
     assert(state == RUNNING || state == RECORDING);
-    char file_name[1024];
-    strcpy(file_name, name);
-    strcat(file_name, ".initial");
-    inital_savedata = fopen(file_name, "r");
-    if (inital_savedata == NULL) {
-        printf("failed to open %s: (%s)\n", file_name, strerror(errno));
+    std::string file_name = name + ".initial";
+    inital_savedata.open(file_name);
+    if (!inital_savedata.is_open()) {
+        printf("failed to open %s\n", file_name.c_str());
         return false;
     }
     return true;
 }
 Uint8 readByte(void) {
     assert(state == RUNNING || state == RECORDING);
-    return fgetc(inital_savedata);
+    return inital_savedata.get();
 }
 bool openSaveWriter(void) {
-    char save_file_name[1024];
-    strcpy(save_file_name, name);
-    strcat(save_file_name, ".expected");
+    std::string save_file_name = name + ".expected";
 
     switch (state) {
     case FINISHED:
         return false;
     case RECORDING:
-        recorder.expected = fopen(save_file_name, "w");
-        if (recorder.expected == NULL) {
-            printf("Failed to open %s: %s\n", save_file_name, strerror(errno));
+        recorder.expected.open(save_file_name);
+        if (!recorder.expected.is_open()) {
+            printf("Failed to open %s\n", save_file_name.c_str());
             return false;
         }
         return true;
     case RUNNING:
         if (!runner.completed) return false;
-        runner.expected = fopen(save_file_name, "r");
-        if (runner.expected == NULL) {
-            printf("Failed to open %s: %s\n", save_file_name, strerror(errno));
+        runner.expected.open(save_file_name);
+        if (!runner.expected.is_open()) {
+            printf("Failed to open %s\n", save_file_name.c_str());
             return false;
         }
         return true;
@@ -138,9 +131,11 @@ bool openSaveWriter(void) {
 int writeByte(Uint8 value) {
     switch (state) {
     case RECORDING:
-        return fputc(value, recorder.expected) != EOF;
+        recorder.expected << value;
+        return 1;
+
     case RUNNING: {
-        uint8_t expected = fgetc(runner.expected);
+        uint8_t expected = runner.expected.get();
         if (runner.completed && value != expected) {
             runner.completed = false;
             printf("expected %c (%d) but got %c (%d)\n", expected, expected, value, value);
@@ -153,24 +148,30 @@ int writeByte(Uint8 value) {
     }
 }
 
-#define INTERVAL 100
-
 Uint32 process_next_command(Uint32 interval, void *param) {
     (void)param;
     assert(state == RUNNING);
-    int x, y;
-    if (fscanf(runner.sim_input, "CLICK %d %d\n", &x, &y) == 2) {
+    std::string cmd;
+    runner.sim_input >> cmd;
+    if (cmd == "CLICK") {
+        int x, y;
+        runner.sim_input >> x;
+        runner.sim_input >> y;
         onClick(x, y);
         return interval;
-    } else if (fscanf(runner.sim_input, "ALTCLICK %d %d\n", &x, &y) == 2) {
+    } else if (cmd == "ALTCLICK") {
+        int x, y;
+        runner.sim_input >> x;
+        runner.sim_input >> y;
         onAltClick(x, y);
         return interval;
-    } else if (feof(runner.sim_input)) {
+    } else if (cmd == "") {
         runner.completed = true;
         save();
         return 0;
     } else {
-        fprintf(stderr, "INVALID TEST SAVE FILE");
+        fprintf(stderr, "input sim unexpected \"%s\"\n", cmd.c_str());
+        exit(1);
         return 0;
     }
 }
@@ -181,19 +182,19 @@ static void usage(void) {
 }
 
 static void run() {
-    runner.sim_input = fopen(name, "r");
-    if (runner.sim_input == NULL) {
-        fprintf(stderr, "Failed to open %s (%s)\n", name, strerror(errno));
+    runner.sim_input.open(name);
+    if (!runner.sim_input.is_open()) {
+        fprintf(stderr, "Failed to open %s (%s)\n", name.c_str(), strerror(errno));
         exit(1);
     }
     SDL_AddTimer(INTERVAL, process_next_command, NULL);
 }
 
 static void record() {
-    printf("Creating test at %s\n", name);
-    recorder.file = fopen(name, "w");
-    if (recorder.file == NULL) {
-        fprintf(stderr, "Failed to open %s (%s)\n", name, strerror(errno));
+    printf("Creating test at %s\n", name.c_str());
+    recorder.file.open(name);
+    if (!recorder.file.is_open()) {
+        fprintf(stderr, "Failed to open %s (%s)\n", name.c_str(), strerror(errno));
         exit(1);
     }
 }
